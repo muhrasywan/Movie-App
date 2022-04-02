2688b7629fb4035147c8e84eaca788cf6ec6ed30
Muhammad Rasywan Rustan <muhammadrasywan@gmail.com>
2022-04-02 23:38:54 +0800

    add navigation in onboarding page

--- app/src/main/java/com/akrakan/bwamov/onboarding/OnboardingOneActivity.kt
+++ app/src/main/java/com/akrakan/bwamov/onboarding/OnboardingOneActivity.kt
@@ -5,19 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.akrakan.bwamov.R
 import com.akrakan.bwamov.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_onboarding_one.*
 
 class OnboardingOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_one)
 
-//        btn_home.setOnClickListener{
-//            var intent = Intent(this@OnboardingOneActivity, OnboardingTwoActivity::class.java)
-//            startActivity(intent)
-//        }
-//        btn_daftar.setOnClickListener{
-//            var intent = Intent(this@OnboardingOneActivity, SignInActivity::class.java)
-//            startActivity(intent)
-//        }
+        btn_home.setOnClickListener{
+            var intent = Intent(this@OnboardingOneActivity, OnboardingTwoActivity::class.java)
+            startActivity(intent)
+        }
+        btn_daftar.setOnClickListener{
+            finishAffinity()
+            var intent = Intent(this@OnboardingOneActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/akrakan/bwamov/onboarding/OnboardingThreeActivity.kt
+++ app/src/main/java/com/akrakan/bwamov/onboarding/OnboardingThreeActivity.kt
@@ -1,12 +1,21 @@
 package com.akrakan.bwamov.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.akrakan.bwamov.R
+import com.akrakan.bwamov.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_onboarding_three.*
 
 class OnboardingThreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_three)
+
+        bth_home.setOnClickListener {
+            finishAffinity()
+            var intent = Intent(this@OnboardingThreeActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
